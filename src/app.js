338625f424b060d43ef17f5dcce0294629785efb
joetidee <joe@saeker.co.uk>
2017-03-13 21:37:17 +0000

    Added comment into app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+/*
+* The code below can be removed. It simply provides some examples of ES2015 syntax.
+* */
 module.exports = function({percent = 100, amount}){
     const percentOff = (percent / 100) * amount;
     return percentOff;
